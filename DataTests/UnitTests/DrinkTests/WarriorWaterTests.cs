446feb4e953c2f4e385d831d773b6d43b563f67f
Madison Burch <mburch@ksu.edu>
2020-09-06 23:31:53 -0500

    Finished test library

---
+++ DataTests/UnitTests/DrinkTests/WarriorWaterTests.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Enums;
+
+namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
+{
+    public class WarriorWaterTests
+    {
+        [Fact]
+        public void ShouldIncludeIceByDefault()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.True(ww.Ice);
+        }
+        [Fact]
+        public void ShouldNotIncludeLemonByDefault()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.False(ww.Lemon);
+        }
+        [Fact]
+        public void ShouldBeSmallByDefault()
+        {
+            AretinoAppleJuice ww = new AretinoAppleJuice();
+            Assert.Equal(Size.Small, ww.Size);
+        }
+        [Fact]
+        public void ShouldHaveCorrectPrice()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.Equal(0.0, ww.Price);
+        }
+        [Fact]
+        public void ShouldHaveCorrectCalories()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.Equal(0.0, ww.Calories);
+        }
+        [Fact]
+        public void ShouldBeAbleToSetSize()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = Size.Large;
+            Assert.Equal(Size.Large, ww.Size);
+            ww.Size = Size.Medium;
+            Assert.Equal(Size.Medium, ww.Size);
+            ww.Size = Size.Small;
+            Assert.Equal(Size.Small, ww.Size);
+        }
+        [Fact]
+        public void ShouldBeAbleToSetIce()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Ice = true;
+            Assert.True(ww.Ice);
+            ww.Ice = false;
+            Assert.False(ww.Ice);
+        }
+        [Fact]
+        public void ShouldBeAbleToSetLemon()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Lemon = true;
+            Assert.True(ww.Lemon);
+            ww.Lemon = false;
+            Assert.False(ww.Lemon);
+        }
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void ShouldHaveCorrectSpecialInstructions(bool includeIce, bool includeLemon)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Ice = includeIce;
+            ww.Lemon = includeLemon;
+            if (!includeIce) Assert.Contains("Hold ice", ww.SpecialInstructions);
+            if (includeLemon) Assert.Contains("Add lemon", ww.SpecialInstructions);
+            if (!includeLemon && includeIce) Assert.Empty(ww.SpecialInstructions);
+        }
+        [Theory]
+        [InlineData(Size.Small, "Small Warrior Water")]
+        [InlineData(Size.Medium, "Medium Warrior Water")]
+        [InlineData(Size.Large, "Large Warrior Water")]
+        public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = size;
+            Assert.Equal(name, ww.ToString());
+        }
+    }
+}
--- DataTests/UnitTests/EntreeTests/ThugsTBoneTests.cs
+++ DataTests/UnitTests/EntreeTests/ThugsTBoneTests.cs
@@ -4,7 +4,7 @@
  * Purpose: Test the ThugsTBone.cs class in the Data library
  */
 using Xunit;
-
+using BleakwindBuffet.Data.Entrees;
 using BleakwindBuffet.Data;
 
 namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
@@ -14,21 +14,29 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         [Fact]
         public void ShouldReturnCorrectPrice()
         {
+            ThugsTBone ttb = new ThugsTBone();
+            Assert.Equal(6.44, ttb.Price);
         }
 
         [Fact]
         public void ShouldReturnCorrectCalories()
         {
+            ThugsTBone ttb = new ThugsTBone();
+            Assert.Equal(982.0, ttb.Calories);
         }
 
         [Fact]
         public void ShouldReturnCorrectSpecialInstructions()
         {
+            ThugsTBone ttb = new ThugsTBone();
+            Assert.Empty(ttb.SpecialInstructions);
         }
 
         [Fact]
         public void ShouldReturnCorrectToString()
         {
+            ThugsTBone ttb = new ThugsTBone();
+            Assert.Equal("Thugs T-Bone", ttb.ToString());
         }
     }
 }
