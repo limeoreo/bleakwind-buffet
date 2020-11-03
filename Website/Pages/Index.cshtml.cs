0a22cc38228f3bb2d4c0c46d11d6e83f1c334d80
Madison Burch <mburch@ksu.edu>
2020-11-03 00:38:38 -0600

    ugh

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -5,17 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data;
 
 namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public void OnGet()
         {
