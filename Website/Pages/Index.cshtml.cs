f943f8d3da76c48d9c33822a7f1ac455079871b7
Madison Burch <mburch@ksu.edu>
2020-11-09 01:09:03 -0600

    finished adding search

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -6,16 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using BleakwindBuffet.Data;
+using BleakwindBuffet;
 
 namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
-        
+        public IEnumerable<IOrderItem> Items { get; protected set; }
+        public string SearchTerms { get; set; }
+        public string[] Categories { get; set; }
 
-        public void OnGet()
+        public void OnGet(string SearchTerms, string[] Categories, int CalMin, int CalMax, double PriceMin, double PriceMax )
         {
-
+            Items = Menu.Search(SearchTerms);
+            Items = Menu.FilterByCategory(Items, Categories);
+            Items = Menu.FilterByCalories(Items, CalMin, CalMax);
+            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
         }
     }
 }
